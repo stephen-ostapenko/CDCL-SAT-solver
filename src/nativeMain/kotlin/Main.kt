d09005f8f3f6c08fa9214a57f561cf80c781f760
sergeykhargelia <sergey.khargelia@gmail.com>
2022-06-01 20:41:11 +0300

    implemented BCP

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import platform.posix.fopen
+
 data class Literal(val variable: String, val hasNegation: Boolean)
 typealias Clause = List<Literal>
 typealias Formula = MutableList<Clause>
@@ -9,21 +11,38 @@ fun getAllVars(f: Formula): List<String> {
     return f.flatten().map { it.variable }.distinct()
 }
 
-fun hasUnassignedVars(allVars: List<String>, state: State): Boolean {
+fun formulaHasUnassignedVars(allVars: List<String>, state: State): Boolean {
     return state.size < allVars.size
 }
 
-fun BCP(f: Formula, state: State): Boolean {
-    TODO()
+fun clauseHasUnassignedVars(clause: Clause, state: State): Boolean {
+    return clause.any { !state.containsKey(it.variable) }
 }
 
-fun hasUnresolvedVariables(clause: Clause, state: State): Boolean {
-    return clause.any { literal -> !state.containsKey(literal.variable) }
+fun BCP(f: Formula, state: State, level: Int): Boolean {
+    while (true) {
+        var foundUnitClause = false
+        for ((id, clause) in f.withIndex()) {
+            val unresolvedLiterals = clause.filter { !state.containsKey(it.variable) }
+            if (unresolvedLiterals.size == 1) {
+                foundUnitClause = true
+                state[unresolvedLiterals.first().variable] = VariableInfo(unresolvedLiterals.first().hasNegation, id, level)
+            }
+        }
+
+        if (!foundUnitClause) {
+            break
+        }
+    }
+
+    return f.all { clauseHasUnassignedVars(it, state) || it.any { literal ->
+        state.get(literal.variable)?.value == literal.hasNegation }
+    }
 }
 
 fun decideNewValue(f: Formula, state: State, level: Int) {
     val score = mutableMapOf<Literal, Int>()
-    f.filter { hasUnresolvedVariables(it, state) }.flatten().forEach { literal ->
+    f.filter { clauseHasUnassignedVars(it, state) }.flatten().forEach { literal ->
         if (!state.containsKey(literal.variable)) {
             val newScore = score.getOrElse(literal, {0} ) + 1
             score[literal] = newScore
@@ -34,28 +53,28 @@ fun decideNewValue(f: Formula, state: State, level: Int) {
 }
 
 fun CDCL(f: Formula): Pair<Boolean, State> {
-    val values = mutableMapOf<String, VariableInfo>()
+    val state = mutableMapOf<String, VariableInfo>()
     val allVars = getAllVars(f)
-    if (!BCP(f, values)) {
+    if (!BCP(f, state, 0)) {
         return Pair(false, mutableMapOf<String, VariableInfo>())
     }
 
     var level = 0
-    while (hasUnassignedVars(allVars, values)) {
+    while (formulaHasUnassignedVars(allVars, state)) {
         level++
-        decideNewValue(f, values, level)
-        while (BCP(f, values) == false) {
+        decideNewValue(f, state, level)
+        while (BCP(f, state, level) == false) {
             /*(backLevel, learntClause) = analyzeConflict()
             f.add(learntClause)
             if (backLevel < 0) {
                 return Pair(false, mutableMapOf<String, VariableInfo>())
             } else {
-                goBack(f, values, backLevel)
+                goBack(f, state, backLevel)
                 level = backLevel
             }*/
         }
     }
-    return Pair(true, values)
+    return Pair(true, state)
 }
 
 @Suppress("OPT_IN_IS_NOT_ENABLED")
