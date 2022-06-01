69fe506d10a6bb8d3e1ca2296f705c6dc0b279f5
Stephen Ostapenko <stepanos2002@gmail.com>
2022-06-02 01:05:26 +0300

    cosmetic

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -28,7 +28,7 @@ fun BCP(f: Formula, state: State, level: Int): Boolean {
     while (true) {
         var foundUnitClause = false
         for ((id, clause) in f.withIndex()) {
-            if (clause.any { literal -> !literal.hasNegation == state.get(literal.variableName)?.value }) {
+            if (clause.any { literal -> !literal.hasNegation == state[literal.variableName]?.value }) {
                 continue
             }
             val unresolvedLiterals = clause.filter { !state.containsKey(it.variableName) }
@@ -52,7 +52,7 @@ fun decideNewValue(f: Formula, state: State, level: Int) {
     val score = mutableMapOf<Literal, Int>()
     f.filter { clauseHasUnassignedVars(it, state) }.flatten().forEach { literal ->
         if (!state.containsKey(literal.variableName)) {
-            val newScore = score.getOrElse(literal, {0} ) + 1
+            val newScore = score.getOrElse(literal) { 0 } + 1
             score[literal] = newScore
         }
     }
@@ -61,11 +61,12 @@ fun decideNewValue(f: Formula, state: State, level: Int) {
     state[decidedLiteral.variableName] = VariableInfo(!decidedLiteral.hasNegation, null, level)
 }
 
+@Suppress("UNUSED_PARAMETER")
 fun goBack(f: Formula, state: State, newLevel: Int): State =
     state.filter { (_, info) -> (info.level <= newLevel) }.toMutableMap()
 
 fun oneLiteralAtLevel(clause: Clause, state: State, level: Int): Boolean =
-    clause.count { state.get(it.variableName)?.level == level } == 1
+    clause.count { state[it.variableName]?.level == level } == 1
 
 fun resolve(clause1: Clause, clause2: Clause, v: VarNameType): Clause =
     (clause1 + clause2).filter { it.variableName != v }.distinct()
@@ -80,13 +81,13 @@ fun analyzeConflict(f: Formula, state: State, level: Int): Pair<Int, Clause> {
 
     while (!oneLiteralAtLevel(currentClause, state, level)) {
         val selectedLiteral = currentClause.find {
-            literal -> state.get(literal.variableName)?.level == level && state.get(literal.variableName)?.antecedent != null
+            literal -> state[literal.variableName]?.level == level && state[literal.variableName]?.antecedent != null
         } ?: break
-        val previousClauseIndex = state.get(selectedLiteral.variableName)?.antecedent ?: throw IllegalArgumentException()
+        val previousClauseIndex = state[selectedLiteral.variableName]?.antecedent ?: throw IllegalArgumentException()
         currentClause = resolve(f[previousClauseIndex], currentClause, selectedLiteral.variableName)
     }
 
-    val newLevel = currentClause.map { state.get(it.variableName)?.level ?: -1 }.filter {it != level}.
+    val newLevel = currentClause.map { state[it.variableName]?.level ?: -1 }.filter {it != level}.
         maxByOrNull { it } ?: -1
     return Pair(newLevel, currentClause)
 }
