c41af0a2c25191bc33937e4a55bf3833e0f4e214
sergeykhargelia <sergey.khargelia@gmail.com>
2022-06-01 20:11:26 +0300

    implemented decide new variable function

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -6,13 +6,7 @@ typealias State = MutableMap<String, VariableInfo>
 data class VariableInfo(var value: Boolean, val antecedent: Int?, val level: Int)
 
 fun getAllVars(f: Formula): List<String> {
-    val vars: MutableList<String> = mutableListOf<String>()
-    for (clause in f) {
-        for (literal in clause) {
-            vars.add(literal.variable)
-        }
-    }
-    return vars.toList()
+    return f.flatten().map { it.variable }.distinct()
 }
 
 fun hasUnassignedVars(allVars: List<String>, state: State): Boolean {
@@ -23,12 +17,24 @@ fun BCP(f: Formula, state: State): Boolean {
     TODO()
 }
 
-fun decideNewValue(f: Formula, state: State) {
-    TODO()
+fun hasUnresolvedVariables(clause: Clause, state: State): Boolean {
+    return clause.any { literal -> !state.containsKey(literal.variable) }
+}
+
+fun decideNewValue(f: Formula, state: State, level: Int) {
+    val score = mutableMapOf<Literal, Int>()
+    f.filter { hasUnresolvedVariables(it, state) }.flatten().forEach { literal ->
+        if (!state.containsKey(literal.variable)) {
+            val newScore = score.getOrElse(literal, {0} ) + 1
+            score[literal] = newScore
+        }
+    }
+    val (decidedLiteral, _) = score.maxByOrNull { (_, score) -> score } ?: return
+    state[decidedLiteral.variable] = VariableInfo(!decidedLiteral.hasNegation, null, level)
 }
 
 fun CDCL(f: Formula): Pair<Boolean, State> {
-    val values: MutableMap<String, VariableInfo> = mutableMapOf<String, VariableInfo>()
+    val values = mutableMapOf<String, VariableInfo>()
     val allVars = getAllVars(f)
     if (!BCP(f, values)) {
         return Pair(false, mutableMapOf<String, VariableInfo>())
@@ -37,12 +43,12 @@ fun CDCL(f: Formula): Pair<Boolean, State> {
     var level = 0
     while (hasUnassignedVars(allVars, values)) {
         level++
-        decideNewValue(f, values)
+        decideNewValue(f, values, level)
         while (BCP(f, values) == false) {
             /*(backLevel, learntClause) = analyzeConflict()
             f.add(learntClause)
             if (backLevel < 0) {
-                return Pair(false, mutableMapOf<String, Boolean>())
+                return Pair(false, mutableMapOf<String, VariableInfo>())
             } else {
                 goBack(f, values, backLevel)
                 level = backLevel
