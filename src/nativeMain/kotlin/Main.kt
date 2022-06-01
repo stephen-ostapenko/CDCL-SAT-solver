cad59a598c9d86b23b2e8efc5e8cda3c0ac8acc5
Stephen Ostapenko <stepanos2002@gmail.com>
2022-06-01 23:30:28 +0300

    getting up to date

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -5,58 +5,115 @@ import mkn.mathlog.utils.DIMACSParser
 import mkn.mathlog.utils.FileInput
 
 typealias VarNameType = Int
-data class Literal(val variable: VarNameType, val hasNegation: Boolean)
+data class Literal(val variableName: VarNameType, val hasNegation: Boolean)
+
 typealias Clause = List<Literal>
 typealias Formula = MutableList<Clause>
-typealias State = MutableMap<String, VariableInfo>
 
 data class VariableInfo(var value: Boolean, val antecedent: Int?, val level: Int)
+typealias State = MutableMap<VarNameType, VariableInfo>
 
 fun getAllVars(f: Formula): List<VarNameType> {
-    val vars = mutableListOf<VarNameType>()
-    for (clause in f) {
-        for (literal in clause) {
-            vars.add(literal.variable)
-        }
-    }
-    return vars.toList()
+    return f.flatten().map { it.variableName }.distinct()
 }
 
-fun hasUnassignedVars(allVars: List<VarNameType>, state: State): Boolean {
+fun formulaHasUnassignedVars(allVars: List<VarNameType>, state: State): Boolean {
     return state.size < allVars.size
 }
 
-fun BCP(f: Formula, state: State): Boolean {
-    TODO()
+fun clauseHasUnassignedVars(clause: Clause, state: State): Boolean {
+    return clause.any { !state.containsKey(it.variableName) }
 }
 
-fun decideNewValue(f: Formula, state: State) {
-    TODO()
+fun BCP(f: Formula, state: State, level: Int): Boolean {
+    while (true) {
+        var foundUnitClause = false
+        for ((id, clause) in f.withIndex()) {
+            val unresolvedLiterals = clause.filter { !state.containsKey(it.variableName) }
+            if (unresolvedLiterals.size == 1) {
+                foundUnitClause = true
+                state[unresolvedLiterals.first().variableName] = VariableInfo(unresolvedLiterals.first().hasNegation, id, level)
+            }
+        }
+
+        if (!foundUnitClause) {
+            break
+        }
+    }
+
+    return f.all { clauseHasUnassignedVars(it, state) || it.any { literal ->
+        state[literal.variableName]?.value == literal.hasNegation
+    }}
 }
 
-fun CDCL(f: Formula): Pair<Boolean, State> {
-    val values: MutableMap<String, VariableInfo> = mutableMapOf<String, VariableInfo>()
+fun decideNewValue(f: Formula, state: State, level: Int) {
+    val score = mutableMapOf<Literal, Int>()
+    f.filter { clauseHasUnassignedVars(it, state) }.flatten().forEach { literal ->
+        if (!state.containsKey(literal.variableName)) {
+            val newScore = score.getOrElse(literal, {0} ) + 1
+            score[literal] = newScore
+        }
+    }
+
+    val (decidedLiteral, _) = score.maxByOrNull { (_, score) -> score } ?: return
+    state[decidedLiteral.variableName] = VariableInfo(!decidedLiteral.hasNegation, null, level)
+}
+
+fun goBack(f: Formula, state: State, newLevel: Int): State {
+    return state.filter { (_, info) -> (info.level <= newLevel) }.toMutableMap()
+}
+
+fun oneLiteralAtLevel(clause: Clause, state: State, level: Int): Boolean {
+    return clause.count { state.get(it.variableName)?.level == level } == 1
+}
+
+fun resolve(clause1: Clause, clause2: Clause, v: VarNameType): Clause {
+    return (clause1 + clause2).filter { it.variableName != v }
+}
+
+fun analyzeConflict(f: Formula, state: State, level: Int): Pair<Int, Clause> {
+    if (level == 0) {
+        return Pair(-1, listOf())
+    }
+    var currentClause = f.find { !clauseHasUnassignedVars(it, state) && it.all { literal ->
+        state[literal.variableName]?.value != literal.hasNegation
+    }} ?: throw IllegalArgumentException()
+
+    while (!oneLiteralAtLevel(currentClause, state, level)) {
+        val selectedLiteral = currentClause.find { literal -> state.get(literal.variableName)?.level == level } ?: throw IllegalArgumentException()
+        val previousClauseIndex = state[selectedLiteral.variableName]?.antecedent ?: throw IllegalArgumentException()
+        currentClause = resolve(f[previousClauseIndex], currentClause, selectedLiteral.variableName)
+    }
+
+    val newLevel = currentClause.map { state[it.variableName]?.level ?: 0 }.maxByOrNull {
+        if (it == level) 0 else it
+    } ?: -1
+    return Pair(newLevel, currentClause)
+}
+
+fun runCDCLSolver(f: Formula): Pair<Boolean, State> {
+    var state = mutableMapOf<VarNameType, VariableInfo>()
     val allVars = getAllVars(f)
-    if (!BCP(f, values)) {
-        return Pair(false, mutableMapOf<String, VariableInfo>())
+    if (!BCP(f, state, 0)) {
+        return Pair(false, mutableMapOf())
     }
 
     var level = 0
-    while (hasUnassignedVars(allVars, values)) {
+    while (formulaHasUnassignedVars(allVars, state)) {
         level++
-        decideNewValue(f, values)
-        while (BCP(f, values) == false) {
-            /*(backLevel, learntClause) = analyzeConflict()
+        decideNewValue(f, state, level)
+        while (!BCP(f, state, level)) {
+            val (backLevel, learntClause) = analyzeConflict(f, state, level)
             f.add(learntClause)
             if (backLevel < 0) {
-                return Pair(false, mutableMapOf<String, Boolean>())
+                return Pair(false, mutableMapOf())
             } else {
-                goBack(f, values, backLevel)
+                state = goBack(f, state, backLevel)
                 level = backLevel
-            }*/
+            }
         }
     }
-    return Pair(true, values)
+    return Pair(true, state)
 }
 
 fun main(args: Array<String>) {
@@ -80,12 +137,25 @@ fun main(args: Array<String>) {
         }
 
         val formula = DIMACSParser.getFormula(inputText)
-
         formula.forEach {
-            println(it)
+            println(it.joinToString(" v ") { (if (it.hasNegation) "~" else "") + it.variableName.toString() })
+        }
+
+        val (sat, interp) = runCDCLSolver(formula)
+        if (sat) {
+            println("satisfiable")
+        } else {
+            println("unsatisfiable")
+        }
+
+        if (sat) {
+            interp.forEach {
+                println("${it.key} <- ${it.value.value}")
+            }
         }
 
     } catch (e: Exception) {
+        println(e::class.qualifiedName)
         println(e.message)
     }
 }
