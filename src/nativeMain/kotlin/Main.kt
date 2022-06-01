ba97ae37df6d007a228534d4c983fdbe08b04469
sergeykhargelia <sergey.khargelia@gmail.com>
2022-06-01 23:40:39 +0300

    fix a bug

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,23 +1,22 @@
 import platform.posix.fopen
 
-data class Literal(val variable: String, val hasNegation: Boolean)
+typealias VarNameType = Int
+data class Literal(val variable: VarNameType, val hasNegation: Boolean)
+
 typealias Clause = List<Literal>
 typealias Formula = MutableList<Clause>
-typealias State = MutableMap<String, VariableInfo>
+typealias State = MutableMap<VarNameType, VariableInfo>
 
 data class VariableInfo(var value: Boolean, val antecedent: Int?, val level: Int)
 
-fun getAllVars(f: Formula): List<String> {
-    return f.flatten().map { it.variable }.distinct()
-}
+fun getAllVars(f: Formula): List<VarNameType> =
+    f.flatten().map { it.variable }.distinct()
 
-fun formulaHasUnassignedVars(allVars: List<String>, state: State): Boolean {
-    return state.size < allVars.size
-}
+fun formulaHasUnassignedVars(allVars: List<VarNameType>, state: State): Boolean =
+    state.size < allVars.size
 
-fun clauseHasUnassignedVars(clause: Clause, state: State): Boolean {
-    return clause.any { !state.containsKey(it.variable) }
-}
+fun clauseHasUnassignedVars(clause: Clause, state: State): Boolean =
+    clause.any { !state.containsKey(it.variable) }
 
 fun BCP(f: Formula, state: State, level: Int): Boolean {
     while (true) {
@@ -26,7 +25,7 @@ fun BCP(f: Formula, state: State, level: Int): Boolean {
             val unresolvedLiterals = clause.filter { !state.containsKey(it.variable) }
             if (unresolvedLiterals.size == 1) {
                 foundUnitClause = true
-                state[unresolvedLiterals.first().variable] = VariableInfo(unresolvedLiterals.first().hasNegation, id, level)
+                state[unresolvedLiterals.first().variable] = VariableInfo(!unresolvedLiterals.first().hasNegation, id, level)
             }
         }
 
@@ -36,7 +35,7 @@ fun BCP(f: Formula, state: State, level: Int): Boolean {
     }
 
     return f.all { clauseHasUnassignedVars(it, state) || it.any { literal ->
-        state.get(literal.variable)?.value == literal.hasNegation
+        state.get(literal.variable)?.value != literal.hasNegation
     }}
 }
 
@@ -53,24 +52,21 @@ fun decideNewValue(f: Formula, state: State, level: Int) {
     state[decidedLiteral.variable] = VariableInfo(!decidedLiteral.hasNegation, null, level)
 }
 
-fun goBack(f: Formula, state: State, newLevel: Int): State {
-    return state.filter { (_, info) -> (info.level <= newLevel) }.toMutableMap()
-}
+fun goBack(f: Formula, state: State, newLevel: Int): State =
+    state.filter { (_, info) -> (info.level <= newLevel) }.toMutableMap()
 
-fun oneLiteralAtLevel(clause: Clause, state: State, level: Int): Boolean {
-    return clause.count { state.get(it.variable)?.level == level } == 1
-}
+fun oneLiteralAtLevel(clause: Clause, state: State, level: Int): Boolean =
+    clause.count { state.get(it.variable)?.level == level } == 1
 
-fun resolve(clause1: Clause, clause2: Clause, v: String): Clause {
-    return (clause1 + clause2).filter { it.variable != v }
-}
+fun resolve(clause1: Clause, clause2: Clause, v: VarNameType): Clause =
+    (clause1 + clause2).filter { it.variable != v }
 
 fun analyzeConflict(f: Formula, state: State, level: Int): Pair<Int, Clause> {
     if (level == 0) {
         return Pair(-1, listOf())
     }
     var currentClause = f.find { !clauseHasUnassignedVars(it, state) && it.all { literal ->
-        state.get(literal.variable)?.value != literal.hasNegation
+        state.get(literal.variable)?.value == literal.hasNegation
     }} ?: throw IllegalArgumentException()
 
     while (!oneLiteralAtLevel(currentClause, state, level)) {
@@ -86,7 +82,7 @@ fun analyzeConflict(f: Formula, state: State, level: Int): Pair<Int, Clause> {
 }
 
 fun CDCL(f: Formula): Pair<Boolean, State> {
-    var state = mutableMapOf<String, VariableInfo>()
+    var state = mutableMapOf<VarNameType, VariableInfo>()
     val allVars = getAllVars(f)
     if (!BCP(f, state, 0)) {
         return Pair(false, mutableMapOf())
@@ -113,6 +109,35 @@ fun CDCL(f: Formula): Pair<Boolean, State> {
 @Suppress("OPT_IN_IS_NOT_ENABLED")
 @OptIn(ExperimentalStdlibApi::class)
 fun main() {
-    println("Hello, Kotlin/Native!")
-    println(isExperimentalMM())
+    /*val f: Formula = mutableListOf(
+        listOf(Literal(1, false), Literal(31, false), Literal(2, true)),
+        listOf(Literal(1, false), Literal(3, true)),
+        listOf(Literal(2, false), Literal(3, false), Literal(4, false)),
+        listOf(Literal(4, true), Literal(5, true)),
+        listOf(Literal(21, false), Literal(4, true), Literal(6, true)),
+        listOf(Literal(5, false), Literal(6, false))
+    )*/
+    /*val f: Formula = mutableListOf(
+        listOf(Literal(1, false)),
+        listOf(Literal(1, false))
+    )*/
+    /*val f: Formula = mutableListOf(
+        listOf(Literal(1, false), Literal(2, false), Literal(3, false)),
+        listOf(Literal(1, false), Literal(2, false), Literal(3, true)),
+        listOf(Literal(1, false), Literal(2, true), Literal(3, false)),
+        listOf(Literal(1, false), Literal(2, true), Literal(3, true)),
+        listOf(Literal(1, true), Literal(2, false), Literal(3, false)),
+        listOf(Literal(1, true), Literal(2, false), Literal(3, true)),
+        listOf(Literal(1, true), Literal(2, true), Literal(3, false)),
+        listOf(Literal(1, true), Literal(2, true), Literal(3, true)),
+    )*/
+    val (isSat, finalState) = CDCL(f)
+    if (isSat) {
+        println("SAT")
+        finalState.forEach { (variable, info) ->
+            println("$variable ${info.value}")
+        }
+    } else {
+        println("UNSAT")
+    }
 }
